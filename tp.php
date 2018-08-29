ad93f15d13877debf284ac3ba65c6d5a293409d5
felpscg <felipefelpgomes@gmail.com>
2018-08-29 02:47:19 -0600

    delivery-basico

---
+++ tp.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+		<link rel="shortcut icon" href="img/icon/icpr.png">
+        <title>teste</title>
+
+        <!--CSS-->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+
+        <!--JS-->
+        <link type="text/javascript" href="js/basic.js">
+        <script type="text/javascript" src=""></script>
+
+        <!--ICO-->
+        
+
+    </head>
+
+<body>
+	<div id="bloco-e">
+		<div class="b-p b-c"><!--primeiro bloco e assim sucessivamente -->
+		Especial da Semana
+		</div>
+		
+		<div class="b-s b-c">
+			<ul>
+				<li>
+				
+				</li>
+				
+				<li>
+				
+				</li>
+			</ul>
+		</div>
+		
+		<div class="b-t b-c">
+    		<img id="prom" src="img/lagarto.png"/>
+                <p style="margin-top: -3px">
+                Imagem meramente ilustrativa
+                <img  class="info" src="img/info56.png"/>
+            </p>
+		</div>
+	</div>
+	</body>
+</html>
